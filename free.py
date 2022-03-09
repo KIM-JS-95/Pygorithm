dd3d97f7f4fec0c5673988d7495ce6ee70ff8ef3
KIM-JS-95 <baugh248730@gmail.com>
2022-03-09 20:46:17 +0900

    123

--- free.py
+++ free.py
@@ -1,16 +1,15 @@
-# TODO: 볼링공 고르기
+dice = list(map(int, input().split()))
+data = [0] * 7
 
-n, m = map(int, input().split())
-data = list(map(int, input().split()))
-array = [0] * 11
+for i in dice:
+    data[i] += 1
 
-for i in data:
-    array[i] += 1
+answer = 0
+if 3 == max(data):
+    answer = 10000 + (data.index(3)) * 1000
+if max(data) == 2:
+    answer = 1000 + (data.index(2)) * 100
+if max(data) == 1:
+    answer = max(dice) * 100
 
-result =0
-
-for i in range(1, m+1):
-    n -= array[i]
-    result += array[i] * n
-
-print(result)
+print(answer)
