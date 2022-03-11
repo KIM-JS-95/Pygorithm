9d0884c32f1abe520870cde14efaa76b37a3693c
KIM-JS-95 <baugh248730@gmail.com>
2022-03-11 21:22:37 +0900

    git branch -M main

--- free.py
+++ free.py
@@ -1,15 +1,24 @@
-dice = list(map(int, input().split()))
-data = [0] * 7
+from itertools import combinations
 
-for i in dice:
-    data[i] += 1
+n = int(input())
+data = []
+member = [i for i in range(1, n+1)]
 
-answer = 0
-if 3 == max(data):
-    answer = 10000 + (data.index(3)) * 1000
-if max(data) == 2:
-    answer = 1000 + (data.index(2)) * 100
-if max(data) == 1:
-    answer = max(dice) * 100
+for i in range(n):
+    data.append(list(map(int, input().split())))
 
-print(answer)
+
+team = []
+for x in list(combinations(member, n//2)):
+    team.append(x)
+
+min_value = 1e9
+
+for i in range(n//2 -1):
+    for j in range(i+1, n//2):
+        link = data[team[i]][team[j]] + data[team[i]][team[j]]
+        start = data[team[i]][team[j]] + data[team[i]][team[j]]
+        min_value = min(abs(link - start), min_value)
+
+
+print(min_value)
