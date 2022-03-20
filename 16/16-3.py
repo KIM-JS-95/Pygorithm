5f4faf4090207f5fb98475e8405dab9dea94ed4f
KIM-JS-95 <baugh248730@gmail.com>
2022-03-20 15:19:14 +0900

    123

---
+++ 16/16-3.py
@@ -0,0 +1,16 @@
+# TODO: 병사 배치하기 (p. 380)
+#   LIS
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.reverse()
+
+dp =[1] * n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if arr[j] < arr[i] :
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n - max(dp))
