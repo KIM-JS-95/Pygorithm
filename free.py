412b3fe6861b50fd04490eebe567d0138cf41058
KIM-JS-95 <baugh248730@gmail.com>
2022-04-07 11:08:55 +0900

    123

--- free.py
+++ free.py
@@ -1,24 +1,8 @@
-from itertools import combinations
+arr = [1, 4, 2, 5, 3]
 
-n = int(input())
-data = []
-member = [i for i in range(1, n+1)]
-
-for i in range(n):
-    data.append(list(map(int, input().split())))
-
-
-team = []
-for x in list(combinations(member, n//2)):
-    team.append(x)
-
-min_value = 1e9
-
-for i in range(n//2 -1):
-    for j in range(i+1, n//2):
-        link = data[team[i]][team[j]] + data[team[i]][team[j]]
-        start = data[team[i]][team[j]] + data[team[i]][team[j]]
-        min_value = min(abs(link - start), min_value)
-
-
-print(min_value)
+summ = 0
+for i in range(len(arr)):
+    for j in range(1, len(arr) + 1, 2):
+        if (j + i <= len(arr)):
+            summ+=sum(arr[i:i + j])
+print(summ)
